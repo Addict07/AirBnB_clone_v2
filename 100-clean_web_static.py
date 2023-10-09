39fe75382622f9820de801a5e9c6847a81074861
Yousef <yuossefbakier@gmail.com>
2023-10-09 03:20:14 +0300

    Fix Bug'

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,9 +8,15 @@ env.hosts = ['54.237.3.221', '54.162.78.235']
 
 def do_clean(number=0):
     """deletes out-of-date archives"""
-    number = 2 if number == 0 else number + 1
+    number = 1 if number == 0 else number
 
+    # remove local
+    total = local("ls -l versions/*.tgz | wc -l", capture=True).stdout.strip()
+    total = int(total)
     c = "find {} -type f -exec ls -t1 {{}} + | tail -{} | xargs rm -rf"
-    local(c.format('versions/', number))
-    path = '/data/web_static/releases'
-    run(c.format(path, number))
+    local(c.format('versions/*.tgz', total - number))
+
+    # remove Host
+    path = '/data/web_static/releases/web_static_*'
+    total = run("ls -l {} | wc -l".format(path)).stdout.strip()
+    run(c.format(path, total - number))
